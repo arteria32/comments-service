7f4e9075f99518bb5730065e0a827dc1bb64e4b7
arteria <denis.rjabko@mail.ru>
2024-05-02 20:03:26 +0300

    working connection to db

---
+++ backend/database/database.ts
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+import { Pool } from 'pg';
+dotenv.config();
+
+const pool = new Pool();
+
+console.log('pool', pool);
+export const query = async (text: string, params?: any[]) => {
+  const start = Date.now();
+  const res = await pool
+    .query(text, params)
+    .catch((error) => console.warn('error query', error));
+  const duration = Date.now() - start;
+  console.log('executed query', { text, duration, rows: res?.rowCount });
+  return res;
+};
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import express, { Express, Request, Response } from 'express';
 import helmet from 'helmet';
+import * as db from '../database/database';
 
 // make sure to create an .env file in the root of the project!
 dotenv.config();
@@ -15,15 +16,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', async (req: Request, res: Response) => {
+  const rows = await db.query('SELECT NOW()');
+  console.log('rows', rows);
+
   res.send(`
 		<h1>Welcome to Node Express TS API Server!!! </h1>
 		<p>
+		POSTGRES_DB: ${process.env.POSTGRES_DB}
 		NODE_ENV: ${process.env.NODE_ENV}
 		PORT: ${port}
 		</p>
 	`);
 });
 
-app.listen(port, () => {
+app.listen(port, async () => {
   console.log(`Node.JS-Express API 📀 listening at http://localhost:${port}`);
 });
