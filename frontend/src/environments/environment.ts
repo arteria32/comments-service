8009b5c86c3716a102745462b04324d5c7e49a65
arteria <denis.rjabko@mail.ru>
2024-05-07 00:50:20 +0300

    implement dockerization frontend

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  commentsServiceApi: process.env.API_PORT || 'http://localhost:5000',
+  commentsServiceApi: process.env.REACT_APP_API_URL || 'http://localhost:5000',
 };
